1e5e4385df055b899112bafd178cea7873b68889
David Randall <142061108+ilovepi31415@users.noreply.github.com>
2024-12-25 00:41:57 -0500

    Fix local error
    The local repo corrupted, so I manually copied in the better code

--- script.js
+++ script.js
@@ -16,16 +16,30 @@ const btnSub = document.querySelector('#subtract');
 const btnMult = document.querySelector('#multiply');
 const btnDiv = document.querySelector('#divide');
 
+// Flags
+let cleared = true;
+let screenStored = false;
+let activeOperand = null;
+
 btnClear.addEventListener('click', () => {clear()});
 
 const numbers = [btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9];
+const operands = [btnAdd, btnSub, btnMult, btnDiv];
+
 numbers.forEach(btn => {
     btn.addEventListener('click', () => {
         addToScreen(btn.textContent);
     });
 });
 
+operands.forEach(btn => {
+    btn.addEventListener('click', () => {
+        operate(btn.textContent);
+    });
+});
+
 function clear() {
+    cleared = true;
     display.textContent= '0';
 }
 
@@ -33,11 +47,34 @@ function addToScreen(n) {
     if (display.textContent.length > 15) {
         return;
     }
-    if (display.textContent == '0') {
+    cleared = false;
+    if (display.textContent == '0' || screenStored) {
         display.textContent = n;
+        screenStored = false;
         return;
     }
     display.textContent += n;
+}
+
+function operate(op) {
+    if (cleared) {
+        return;
+    }
+    activeOperand = op;
+    operands.forEach(btn => {
+        if (btn.textContent == op) {
+            btn.classList.add('active');
+        }
+        else {
+            btn.classList.remove('active');
+        }
+    });
+    ans = display.textContent;
+    screenStored = true;
+}
+
+function evaluate() {
+    return;
 }
 
 clear();
